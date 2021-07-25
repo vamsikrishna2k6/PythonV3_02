07db3ecefdf76f3050625ded30f451377f8c4ff0
vamsikrishna2k6 <vamsi.krishna2k6@gmail.com>
2021-07-25 02:23:02 -0400

    01

---
+++ srrings.py
@@ -0,0 +1,28 @@
+print("today is a good day to learn python")
+print('python is fun')
+print("python string is easy to use")
+print("hello" + 'there', 3, 1.2 + 2)
+print('heloo')
+print()
+carmake = "honda"
+model = "accord"
+print(carmake + " " + model)
+
+# caryear = input("enter the year")
+# color = input("enter colour")
+# print(carmake, model, caryear + color)
+
+one = """ 1 | Number 1  THe Larch"""
+two = """ 2 | Number 2   The Horse Chestnut"""
+print(one + "\n" + two)
+print()
+print("Number 1 \t The Larch")
+print("Number 2 \t The Horse Chestnut")
+
+
+# escaping the special char using \ othe using raw string 
+
+print(""""hello" , 'hey', htere. """)
+print("hello\"hey,.")
+print("\\home\\vamsi\\PythonV3_02")
+print(r"\home\vamsi\PythonV3_02")
